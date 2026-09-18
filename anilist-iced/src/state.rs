13f45ca50f3f62f02eee7859f434a95681cfae37
000hen <git@muisnowdevs.one>
2026-09-19 01:29:00 +0800

    chore: replace PNG icon with WebP

--- anilist-iced/src/state.rs
+++ anilist-iced/src/state.rs
@@ -19,7 +19,7 @@ use crate::{
     window::{detail, list},
 };
 
-static ICON_BYTES: &[u8] = include_bytes!("./image/icon.png");
+static ICON_BYTES: &[u8] = include_bytes!("./image/icon.webp");
 
 #[derive(Debug, Clone)]
 pub enum WindowType {
